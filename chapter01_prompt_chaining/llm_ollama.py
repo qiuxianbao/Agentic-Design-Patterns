a9b794fefaf19c892b95e355043f033a44bb9f6c
qiuxianbao <qiuxianbao@aipark.com>
2026-04-29 17:13:25 +0800

    feat(prompt chaining): ollama add stream

--- chapter01_prompt_chaining/llm_ollama.py
+++ chapter01_prompt_chaining/llm_ollama.py
@@ -12,8 +12,25 @@ def test_ollama():
     print(response.content)
 
 
+def test_ollama_stream():
+    llm = ChatOllama(
+        model="qwen3:4b",
+        base_url="http://10.100.16.18:11434/",
+        temperature=0.7,
+    )
+    # 如果使用普通模型，可以在提示词中明确要求展示思考过程
+    user_input = "请详细展示你的思考过程，然后再回答：为什么地球是圆的？"
+    
+    print("AI 正在思考并输出：")
+    # 使用 stream 方法流式输出，并且设置 end="" 避免字符串换行
+    for chunk in llm.stream(user_input):
+        print(chunk.content, end="", flush=True)
+    print()
+
+
 def main():
-    test_ollama()
+    # test_ollama()
+    test_ollama_stream()
 
 
 if __name__ == "__main__":
